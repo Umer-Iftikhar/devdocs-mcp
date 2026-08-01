b7c11ebf5a25b9e513379eaacde3cd508ffae9b2
Umer-Iftikhar <umeriftikhar981@gmail.com>
2026-08-01 23:15:54 +0500

    setup: configure MCP server with Streamable HTTP transport

--- DevDocsMcp/Program.cs
+++ DevDocsMcp/Program.cs
@@ -1,6 +1,22 @@
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.
+    AddMcpServer(options =>
+    {
+        options.ServerInfo = new()
+        {
+            Name = "Dev Docs MCP Server",
+            Description = "This server provides MCP functionality to get imformation from the local documents and user's github profile.",
+            Version = "1.0.0",
+        };
+    })
+    .WithHttpTransport()
+    .WithToolsFromAssembly();
+
+
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+app.MapMcp("/mcp");
 
-app.Run();
+
+await app.RunAsync();
